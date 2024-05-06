77553de0a8bc5031a3b465126a6f79583e87063e
JJCO10 <juanjosecarmonaortiz@gmail.com>
2024-05-05 20:19:54 -0500

    pruebas 1

--- react-app-1/src/App.jsx
+++ react-app-1/src/App.jsx
@@ -4,30 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let holaMundo = ["Hola mundo", "Juan José Carmona Ortiz"];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div ClassName="App">
+        {alert("vaaaaamooooo")}
+        <img src={reactLogo} className='img'/>
+        <h1>{holaMundo[0]}</h1>
+        <input value={holaMundo[1]} />
+        <br />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
